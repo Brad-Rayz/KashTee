a27460baca203bcb8009a571445d448154e344cf
Brad-Rayz <bhavikrathod45@gmail.com>
2023-10-13 12:06:41 +0530

    First commit

---
+++ src/qapi.js
@@ -0,0 +1,29 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"6528e45dc5fd0f0020b0a3e9",
+				"6528e45dc5fd0f0020b0a3ec"
+			]
+		},
+		"6528e45dc5fd0f0020b0a3e9": {
+			"id": "6528e45dc5fd0f0020b0a3e9",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"6528e45dc5fd0f0020b0a3ec": {
+			"id": "6528e45dc5fd0f0020b0a3ec",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "create-react-app",
+	"site": {
+		"styles": {},
+		"seo": {}
+	}
+}
